01b68326dce62185789a554acd28b70e22b5c58c
AlterBins <sashap84@mail.ru>
2022-12-26 23:17:46 +0300

    added task 6

---
+++ 6/Program.cs
@@ -0,0 +1,7 @@
+﻿int a;                             
+Console.Write("введите число: ");
+a = int.Parse(Console.ReadLine());
+if (a % 2 == 1)
+    Console.WriteLine("Нет");
+else
+    Console.WriteLine("Да");
