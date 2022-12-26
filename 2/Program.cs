356b2db0d0295764c3a341049b1520a02861eb34
AlterBins <sashap84@mail.ru>
2022-12-26 23:11:29 +0300

    added task 2

---
+++ 2/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = -9;
+int b = -3;
+if(a > b)
+{
+    Console.Write(a);
+    Console.WriteLine(" - большее");
+    Console.Write(b);
+    Console.WriteLine(" - меньшее");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine(" - меньшее");
+    Console.Write(b);
+    Console.WriteLine(" - большее");
+}
