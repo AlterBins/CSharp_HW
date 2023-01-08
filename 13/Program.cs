2f973373e96c13d4cc8ac943c1a3f29278420e2b
AlterBins <sashap84@mail.ru>
2023-01-08 23:10:07 +0300

    added task 13

---
+++ 13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int DigitFind(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
+}
+
+if (DigitFind(number) == -1)
+{
+Console.WriteLine("Третьей цифры не существут");
+}
+else
+{
+Console.WriteLine($"Третья цифра: {DigitFind(number)}");
+}
