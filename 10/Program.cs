0001e54094e3f50ad42caf67817775c9618f23be
AlterBins <sashap84@mail.ru>
2023-01-08 23:08:39 +0300

    added task 10

---
+++ 10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number / 10 % 10;
+
+Console.Write("Вторая цифра: ");
+Console.WriteLine(result);
