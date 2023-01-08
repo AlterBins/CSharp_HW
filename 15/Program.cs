d1c61e0dbf10c3b7e2b60061e6232e0222bd7f8b
AlterBins <sashap84@mail.ru>
2023-01-08 23:11:13 +0300

    added task 15

---
+++ 15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int date = Convert.ToInt32(Console.ReadLine());
+
+if (date == 6 || date == 7)
+{
+    Console.Write("Выходной день");
+}
+else if (date < 1 || date > 7)
+{
+    Console.Write("Такого дня не существует)");
+}
+else
+{
+    Console.Write("Будний день");
+}
