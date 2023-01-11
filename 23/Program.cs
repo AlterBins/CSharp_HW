5ccc226e257f66a3eea31f5ee30bb9c449b0a144
AlterBins <sashap84@mail.ru>
2023-01-12 00:44:27 +0300

    added task 23

---
+++ 23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void Quarto()
+{
+    int count = 1;
+    while (count <= n)
+    {
+    Console.WriteLine($"Число {count} в кубе --> {count * count * count}");
+    count++;
+    }
+}
+Quarto();
