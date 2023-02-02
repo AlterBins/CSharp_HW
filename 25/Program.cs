15be50b51ffdfc1238a1007de64919455ffe7e26
AlterBins <sashap84@mail.ru>
2023-02-02 17:33:36 +0300

    added task 25

---
+++ 25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int Exponentiation(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = num1 * result;
+    }
+    return result;
+}
+int exponentiation = Exponentiation(a, b);
+Console.WriteLine($"-> {exponentiation}");
