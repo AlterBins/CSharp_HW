51fa88bc63dfc0e430fdce660b3c95314e289489
AlterBins <sashap84@mail.ru>
2023-02-02 17:35:42 +0300

    added task 29

---
+++ 29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[8];
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 33);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+RandomArray(array);
+PrintArray(array);
