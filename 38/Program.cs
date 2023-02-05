cf3e922cd89f5a67bf3e42dbfbee1a57264e46db
AlterBins <sashap84@mail.ru>
2023-02-05 22:58:55 +0300

    added task 38

---
+++ 38/Program.cs
@@ -0,0 +1,45 @@
+﻿double[] NewRandowDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rnd.NextDouble() * (max - min) + min);
+    }
+    return arr;
+}
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+double[] array = NewRandowDouble(5, 10, 99);
+PrintArray(array);
+double MaxDifference(double[] arr)
+{
+double maxNumber =arr[0];
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > maxNumber) maxNumber = arr[i];
+}
+return maxNumber;
+}
+double MinDifference(double[] arr)
+{
+double minNumber = arr[0];
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] < minNumber) minNumber = arr[i];
+}
+return minNumber;
+}
+double differenceMax = MaxDifference(array);
+double differenceMin = MinDifference(array);
+double result = differenceMax - differenceMin;
+Console.WriteLine($"-> {Math.Round(result)}");
