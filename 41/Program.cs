6ef8b048814d24895a622ef08c92199c8edc65c4
AlterBins <sashap84@mail.ru>
2023-02-08 23:53:38 +0300

    added task 41

---
+++ 41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите элементы (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int Elements(int[] arr)
+{
+    int numbers = 0;
+    for (int M = 0; M < arr.Length; M++)
+    {
+        if (arr[M] > 0) numbers++;
+    }
+    return numbers;
+}
+int numbers = Elements(arr);
+Console.WriteLine($"-> {numbers}");
