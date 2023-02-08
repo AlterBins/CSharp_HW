3efdd7b42cb295bc5777c9126a0a7df65dbeb330
AlterBins <sashap84@mail.ru>
2023-02-08 23:54:27 +0300

    added task 43

---
+++ 43/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double X(double b1, double k1, double b2, double k2)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    return x;
+}
+double Y(double b1, double k1)
+{
+    double x = X(b1, k1, b2, k2);
+    double y = k1 * x + b1;
+    return x;
+}
+double x = X(b1, k1, b2, k2);
+double y = Y(b1, k1);
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+Console.WriteLine($"-> ({x},{y})");
