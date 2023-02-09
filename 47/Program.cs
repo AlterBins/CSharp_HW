40351975b0f035a263c5980432be1a4012483716
AlterBins <sashap84@mail.ru>
2023-02-09 20:51:20 +0300

    added task 47

---
+++ 47/Program.cs
@@ -0,0 +1,28 @@
+﻿double[,] GreatMatrixRndDouble(int rows, int colums, double min, double max)
+{
+    double[,] matrix = new double[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * (max - min) + min, 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],6}, ");
+            else Console.Write($"{matrix[i, j],6} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+double[,] matrixArray = GreatMatrixRndDouble(3, 4, -1.5, 3.19);
+PrintMatrix(matrixArray);
