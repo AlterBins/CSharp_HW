48b6dc8077bd96849a2288b0e73df814a799fa38
AlterBins <sashap84@mail.ru>
2023-02-15 00:12:13 +0300

    added task 68

---
+++ 68/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите неотрицательное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"-> {FunctionAkkerman(m, n)}");
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAkkerman(m - 1, 1);
+    else return FunctionAkkerman(m - 1, FunctionAkkerman(m, n - 1));
+}
