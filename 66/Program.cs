e5a2f1dc6c101e1b1db71f025a0f0d0bc26be289
AlterBins <sashap84@mail.ru>
2023-02-15 00:11:40 +0300

    added task 66

---
+++ 66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"-> {SummaDigits(m, n)}");
+
+int SummaDigits(int num1, int num2)
+{
+    if (num1 == num2) return num1;
+    else if (num1 < num2) return num2 + SummaDigits(num1, num2 - 1);
+    else return num2 + SummaDigits(num1, num2 + 1);
+}
