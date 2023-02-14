2e75eb7e3029cc45873d786eb6023b7c6a651c3a
AlterBins <sashap84@mail.ru>
2023-02-14 23:46:28 +0300

    added task 54

---
+++ 54/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,] NewRndMatrix(int rows, int columns, int minElement, int maxElement)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(minElement, maxElement + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j],2}, ");
+            else Console.Write($"{matrix[i, j],2}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void SortingElements(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = NewRndMatrix(4, 4, 1, 9);
+Console.WriteLine("До изменения:");
+PrintMatrix(matrix);
+Console.WriteLine();
+SortingElements(matrix);
+Console.WriteLine("После изменения:");
+PrintMatrix(matrix);
